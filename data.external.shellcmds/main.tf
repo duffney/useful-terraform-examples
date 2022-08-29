810284249bd44a640585defff6136e8a910fb303
Josh Duffney <jduffney@microsoft.com>
2022-08-29 19:30:12 +0000

    initial commit

--- data.external.shellcmds/main.tf
+++ data.external.shellcmds/main.tf
@@ -6,12 +6,17 @@ terraform {
 # Pass input and execute a shell script
 data "external" "passInput" {
   program = ["${path.module}/script.sh"]
-    query = {
+  query = {
     name = "abc123"
+    email = "abc123@example.com"
   }
 }
 
 # Output a variable from the shell script
-output "args" {
-  value = data.external.token.result["name"]
+output "name" {
+  value = data.external.passInput.result["name"]
+}
+
+output "email" {
+  value = data.external.passInput.result["email"]
 }
--- data.external.shellcmds/script.sh
+++ data.external.shellcmds/script.sh
@@ -3,11 +3,12 @@
 # script.sh
 
 # parse input as JSON & assign to a variable
-eval "$(jq -r '@sh "name=\(.name)"')"
+eval "$(jq -r '@sh "name=\(.name);email=\(.email)"')"
 
 #return a valid JSON object
 cat <<EOF
 {
-  "name": "$name"
+  "name": "$name",
+  "email": "$email"
 }
 EOF
